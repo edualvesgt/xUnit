3db469cbb238f58aef69481d28861d9d2b905a15
Eduardo Alves Araújo Silva <46291995867@info.local>
2024-08-01 10:52:59 -0300

    Iniciando estudos de Xunit

---
+++ TesteUnitarios/Livros_Codigo/GerenciamentoDeLivros/Livros.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciamentoDeLivros
+{
+    public class Livros
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+
+        public Livros(string titulo, string autor)
+        {
+            // Atribui o valor do parâmetro "titulo" à propriedade "Titulo"
+            Titulo = titulo;
+            // Atribui o valor do parâmetro "autor" à propriedade "Autor"
+            Autor = autor;
+        }
+    }
+}
---
+++ TesteUnitarios/Livros_Codigo/Livros.Test/Livro.Test/Livro_Test.cs
@@ -0,0 +1,30 @@
+﻿using GerenciamentoDeLivros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Livro.Test
+{
+    public class Livro_Test
+    {
+
+        [Fact]
+        public void AdicionarLivro()
+        {
+            // Arrange: Cria uma lista de livros e um livro a ser adicionado
+            var listaDeLivros = new List<Livros>();
+            var livro = new Livros("Pai Rico Pai Pobre", "Robert Kiyosak");
+
+            // Act: Adiciona o livro à lista
+            listaDeLivros.Add(livro);
+
+
+
+            // Assert: Verifica se o livro foi adicionado corretamente à lista
+            Assert.Contains(livro, listaDeLivros);
+        }
+    }
+}
